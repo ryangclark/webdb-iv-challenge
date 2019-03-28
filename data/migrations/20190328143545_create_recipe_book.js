bb7dc4c9c9fcafb397b7212042c4f7f39aeded3e
Clark <rclark010@gmail.com>
2019-03-28 15:13:11 -0600

    First migration complete.

--- data/migrations/20190328143545_create_recipe_book.js
+++ data/migrations/20190328143545_create_recipe_book.js
@@ -1,8 +1,66 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema
+    .createTable('dishes', table => {
+      table.increments();
+      table
+        .string('name', 128)
+        .notNullable()
+        .unique();
+    })
+    .createTable('recipes', table => {
+      table.increments();
+      table.string('name', 128).notNullable();
+      table
+        .integer('dish_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('dishes')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+    })
+    .createTable('instructions', table => {
+      table.increments();
+      table
+        .integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('recipes')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      table.text('content').notNullable();
+    })
+    .createTable('ingredients', table => {
+      table.increments();
+      table
+        .string('name', 128)
+        .notNullable()
+        .unique();
+    })
+    .createTable('recipes_incredients', table => {
+      table.increments();
+      table
+        .integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('recipes')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      table
+        .integer('ingredient_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('ingredients')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      table
+        .integer('quantity')
+        .unsigned()
+        .notNullable();
+    });
 };
 
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = function(knex, Promise) {};
